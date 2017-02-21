1f174fe5bcf5b4aef777619119c4715175e040a1
Micky Shaked <micky.shaked@gmail.com>
2017-02-21 16:50:19 -0500

    update server port for herok

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/build'));
 app.use(express.static(__dirname + '/Stylesheets'));
 app.use(express.static(__dirname + '/build/Assets'));
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('listening on port 3000 or Heroku\'s port');
 });
