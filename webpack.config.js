7d00e0d84eb635491b7a502a3a883340c0489abc
Micky Shaked <micky.shaked@gmail.com>
2017-02-21 10:48:57 -0500

    webpack and package.json configured

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: ['webpack/hot/dev-server','./index.js'],
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'webpack-bundle.js',
+		publicPath: '/build'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devServer: {
+		hot: true,
+		contentBase: './build',
+		inline: true,
+		port: 3000,
+		stats: {
+			colors: true
+		}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/,
+				query: {
+						presets: ['es2015', 'react']
+				}
+			},
+			{
+				test: /(\.css|\.scss)$/,
+				loaders: ['style', 'css', 'sass']
+			},
+      {
+        test: /\.(jpg|eps)$/,
+        loader: "file-loader?name=/Assets/[name].[ext]"
+      }
+		]
+	}
+};
