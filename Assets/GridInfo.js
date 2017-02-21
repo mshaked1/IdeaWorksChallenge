7e75b37013dae1b09d8292eb8b194b97ba33c122
Micky Shaked <micky.shaked@gmail.com>
2017-02-21 10:47:23 -0500

    image assets from photoshop sample

---
+++ Assets/GridInfo.js
@@ -0,0 +1,26 @@
+module.exports = {
+  0: {
+    date: 'FEBRUARY 10 2017',
+    title: 'Bureaux exquisite delightful carefully curated soft power.',
+  },
+  1: {
+    date: 'FEBRUARY 2 2017',
+    title: 'Sharp bureaux sleepy K-pop carefully curated.',
+  },
+  2: {
+    date: 'JANUARY 27 2017',
+    title: 'St Moritz uniforms Beams.',
+  },
+  3: {
+    date: 'JANUARY 21 2017',
+    title: 'Esse airport veniam ryokan soft power.',
+  },
+  4: {
+    date: 'JANUARY 18 2017',
+    title: 'K-pop extraordinary.',
+  },
+  5: {
+    date: 'JANUARY 12 2017',
+    title: 'Artisanal iconic cutting-edge business class.',
+  },
+}
