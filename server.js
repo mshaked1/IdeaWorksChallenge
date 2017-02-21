3aba4fbac7d98ed4cfeb33bae1783a48f3c304d9
Micky Shaked <micky.shaked@gmail.com>
2017-02-21 17:07:27 -0500

    trying to get server to serve index.html file on heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/build'));
 app.use(express.static(__dirname + '/Stylesheets'));
 app.use(express.static(__dirname + '/build/Assets'));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html');
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 app.listen(port, () => {
   console.log('listening on port 3000 or Heroku\'s port');
