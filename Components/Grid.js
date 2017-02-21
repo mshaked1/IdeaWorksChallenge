254d475772eb393f872ea409da4c677dd420b987
Micky Shaked <micky.shaked@gmail.com>
2017-02-21 13:22:58 -0500

    masonry styling updated, mobile media query

--- Components/Grid.js
+++ Components/Grid.js
@@ -5,13 +5,11 @@ import GridInfo from '../Assets/GridInfo';
 let gridItems = Object.keys(GridInfo).map((item) => {
   return GridInfo[item];
 });
-console.log(gridItems);
 class Grid extends Component {
   render() {
     let gridElements = gridItems.map((item, index) => {
       return <GridItem key={index} index={index} date={item.date} title={item.title} />
     });
-    console.log(gridElements);
     return (
       <div className='grid'>
         {gridElements}
